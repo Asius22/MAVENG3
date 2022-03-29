22cdbb4f0eb3eea4e2225ac5f9ba17e9e0546ce6
Asius22 <ciro_2000@live.it>
2022-03-29 15:05:20 +0200

    Aggiunto Log

---
+++ src/main/java/db/Log.java
@@ -0,0 +1,39 @@
+package db;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+public class Log {
+    private static final Logger l = Logger.getLogger(Log.class);
+    private static Log log = null;
+
+    private Log(){}
+
+    {
+        BasicConfigurator.configure();
+    }
+
+    public static Log getInstance(){
+        if (log == null) synchronized (Log.class){
+            log = new Log();
+        }
+        return log;
+    }
+
+    public void error(String parameter){
+        l.error(parameter);
+    }
+
+    public void warn(String parameter){
+        l.warn(parameter);
+
+    }
+
+    public void info(String parameter){
+        l.info(parameter);
+    }
+
+    public void debug(String parameter){
+        l.debug(parameter);
+    }
+}
---
+++ src/main/java/db/TestRead.java
@@ -0,0 +1,24 @@
+package db;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
+public class TestRead {
+    public static void main(String[] args) {
+        Log l = Log.getInstance();
+        DBConnector db = DBConnector.getIstance();
+        String query = "SELECT * FROM studente";
+        try {
+            PreparedStatement stat = db.getPreparedStatement(query);
+            ResultSet res = stat.executeQuery();
+            ResultSetMetaData md = res.getMetaData();
+            while (res.next())
+                for (int i = 1; i <= md.getColumnCount(); i++) {
+                    l.info(res.getString(i) + "\n");
+                }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
