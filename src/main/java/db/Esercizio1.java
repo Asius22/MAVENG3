d73bd94877e5e7644ea9ab91be1c0b6cc6708ccb
Asius22 <ciro_2000@live.it>
2022-03-29 16:29:16 +0200

    Completato esercizio 1

--- src/main/java/db/Esercizio1.java
+++ src/main/java/db/Esercizio1.java
@@ -7,18 +7,30 @@ public class Esercizio1 {
 
     public static void main(String[] args) throws SQLException {
         DBConnector connector = DBConnector.getIstance();
-        String query = "CREATE TABLE `jdbc`.`prodotto` (" +
-                "  id INT NOT NULL," +
-                "  marca VARCHAR(45) NOT NULL," +
-                "  tipo VARCHAR(20) NOT NULL," +
-                "  modello VARCHAR(20) NOT NULL," +
-                "  costo INT NOT NULL," +
-                "  PRIMARY KEY (`ID`));";
-
+        String query = connector.getReader().getProp().getProperty("create");
+        String insert = connector.getReader().getProp().getProperty("insert");
+        String delete = connector.getReader().getProp().getProperty("drop");
         int res = -1;
+        boolean delRes = false;
+        try (PreparedStatement stat = connector.getPreparedStatement(delete)) {
+            delRes = stat.execute();
+        } catch (SQLException e) {
+            connector.getLog().error(
+                    e.getSQLState() +
+                            " " +
+                            e.getErrorCode() +
+                            "\n" +
+                            e.getMessage());
+        } finally {
+            connector.getLog().info(
+                    !delRes ? "Eliminazione riuscita" : "Si prega di riprovare"
+            );
+        }
+
         try (PreparedStatement stat = connector.getPreparedStatement(query)) {
             res = stat.executeUpdate();
         } catch (SQLException e) {
+            e.printStackTrace();
             connector.getLog().error(
                     e.getSQLState() +
                             " " +
@@ -29,7 +41,31 @@ public class Esercizio1 {
             connector.getLog().info(
                     res == 0 ? "Creazione riuscita" : "Si prega di riprovare"
             );
-            connector.closeConnection();
         }
+
+        try (PreparedStatement stat = connector.getPreparedStatement(insert)) {
+            stat.setInt(1, 1);
+            stat.setString(2, "Samsung");
+            stat.setString(3, "telefono");
+            stat.setString(4, "s22");
+            stat.setInt(5, 1200);
+
+            res = stat.executeUpdate();
+        } catch (SQLException e) {
+            connector.getLog().error(
+                    e.getSQLState() +
+                            " " +
+                            e.getErrorCode() +
+                            "\n" +
+                            e.getMessage());
+        } finally {
+            connector.getLog().info(
+                    res > 0 ? "inserimento riuscito" : "Si prega di riprovare"
+            );
+            connector.closeConnection();
+
+        }
+
+
     }
 }
