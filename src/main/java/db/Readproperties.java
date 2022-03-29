2d8a96a8470fa8bbf8ac2648ecd6e0bf5b50a17b
Asius22 <ciro_2000@live.it>
2022-03-29 15:58:05 +0200

    pulito codice

--- src/main/java/db/Readproperties.java
+++ src/main/java/db/Readproperties.java
@@ -7,10 +7,16 @@ import java.io.InputStream;
 import java.util.Properties;
 import java.util.Scanner;
 
+/**
+ * prodotto
+ * marca tipo modello costo
+ *
+ * lettura dati file
+ */
 public class Readproperties {
     private static final Log l = Log.getInstance();
-    private Properties prop;
     private static final String resourceName = "application.properties";
+    private Properties prop;
     private InputStream input;
     private ClassLoader loader = Thread.currentThread().getContextClassLoader();
 
@@ -21,41 +27,38 @@ public class Readproperties {
     public Readproperties(Properties prop, InputStream input) {
         this.prop = prop;
         this.input = input;
+
     }
 
-    public Readproperties(){}
-
-    /**
-     *
-     */
-    public void init() {
-        this.prop = new Properties();
+    public Readproperties() throws IOException {
+        init();
     }
 
     /**
      *
      * @throws IOException
      */
-    public void read() throws IOException {
+    public void init() throws IOException{
+        this.prop = new Properties();
         input = new FileInputStream(
                 "src\\main\\resources\\" + resourceName
         );
         this.prop.load(input);
     }
 
-    public static void main(String[] args) {
-        Readproperties readproperties = new Readproperties();
-        readproperties.init();
-        try{
-            readproperties.read();
+    public Properties getProp() {
+        return prop;
+    }
 
-            String user = readproperties.prop.getProperty("db.url");
+    public void setProp(Properties prop) {
+        this.prop = prop;
+    }
 
-            System.out.println(user);
-        } catch (IOException e){
-            e.printStackTrace();
-            l.error(e.getMessage());
-        }
+    public InputStream getInput() {
+        return input;
+    }
 
+    public void setInput(InputStream input) {
+        this.input = input;
     }
 }
