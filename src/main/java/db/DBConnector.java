2bd426358527ce7a59eee44b3afe1895966c459a
Asius22 <ciro_2000@live.it>
2022-03-29 15:57:47 +0200

    aggiunta lettura proprietà

--- src/main/java/db/DBConnector.java
+++ src/main/java/db/DBConnector.java
@@ -1,30 +1,45 @@
 package db;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnector {
+    private static final Log l = Log.getInstance();
+    private static final String DB_USER = "db.username", DB_URL = "db.url", DB_PWD = "db.password", DB_CLASS = "db.class";
     private static DBConnector connector;
     private Connection connection;
-    public static final String DB_USER = "root",
-            DB_PWD = "123456sette",
-            DB_URL = "jdbc:mysql://localhost/JDBC?",
-            DB_CLASS = "com.mysql.cj.jdbc.Driver";
+    private Readproperties properties;
 
     private DBConnector() {
         createConnection();
+        try {
+            properties = new Readproperties();
+        } catch (IOException e){
+            e.printStackTrace();
+            l.error(e.getMessage());
+        }
         connector = this;
     }
 
     private void createConnection() {
         if (connection == null) {
+            Properties prop = properties.getProp();
             try {
-                Class.forName(DB_CLASS).getDeclaredConstructor().newInstance();
-                connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PWD);
+                Class.forName(prop.getProperty(DB_CLASS))
+                        .getDeclaredConstructor()
+                        .newInstance();
+                connection = DriverManager.getConnection(
+                        prop.getProperty(DB_URL),
+                        prop.getProperty(DB_USER),
+                        prop.getProperty(DB_PWD)
+                );
             } catch (Exception e) {
                 e.printStackTrace();
+                l.error(e.getMessage());
             }
         }
     }
