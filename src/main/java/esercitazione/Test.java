d6108089e213432c2d8f0b8f0469a14501999618
Asius22 <ciro_2000@live.it>
2022-03-30 12:24:35 +0200

    Runnato progetto

--- src/main/java/esercitazione/Test.java
+++ src/main/java/esercitazione/Test.java
@@ -1,11 +1,58 @@
 package esercitazione;
 
+import db.DBConnector;
+
+import java.sql.SQLException;
+
 /**
  * anagrafica (id, nome, cognome, eta citta provincia cap)
  * scrivere un programma java che effettui l'inserimento dei nominativi e stampi i record
  * utilizzare i thread
  *
  * thread runnable che ha prepared statement e lo usa?
+ * il main si occupa della creazione della tabella
  */
 public class Test {
+    public static void main(String[] args) {
+        String create = "CREATE TABLE IF NOT EXISTS ANAGRAFICA(" +
+                "id int not null, " +
+                "nome varchar(20) not null, " +
+                "cognome varchar(20) not null, " +
+                "eta int not null, " +
+                "citta varchar(20) not null, " +
+                "provincia varchar(3) not null, " +
+                "cap int not null, " +
+                "primary key (id));";
+
+        String insert = "INSERT INTO ANAGRAFICA (id, nome, cognome, eta, citta, provincia, cap) values (?,?,?,?,?,?,?)";
+        String read = "SELECT * FROM ANAGRAFICA";
+
+        DBConnector connector = DBConnector.getIstance();
+        try{
+            Thread creationThread = new Thread(new InsertStatementThread(
+                    connector.getPreparedStatement(create),
+                    true));
+
+            Thread insertThread = new Thread( new InsertStatementThread(
+                    connector.getPreparedStatement(insert),
+                    false
+            ));
+
+            Thread readThread = new Thread(
+                    new SimpleStatementThread(
+                            connector.getPreparedStatement(read)
+                    )
+            );
+
+            creationThread.start();
+            //insertThread.start();
+            readThread.start();
+
+        } catch (SQLException e){
+            connector.getLog().error(e.getMessage());
+            e.printStackTrace();
+        }
+
+
+    }
 }
