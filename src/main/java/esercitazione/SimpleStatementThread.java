1eb036a3a7eca781988b157e8d9113e46e5ea19e
Asius22 <ciro_2000@live.it>
2022-03-30 12:23:13 +0200

    Creata gerarchia di sqlThread

---
+++ src/main/java/esercitazione/SimpleStatementThread.java
@@ -0,0 +1,35 @@
+package esercitazione;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * questa classe crea un nuovo metodo che si occupa di leggere la tabella
+ */
+public class SimpleStatementThread extends StatementThread{
+    /**
+     *
+     * @param stat
+     */
+    public SimpleStatementThread(PreparedStatement stat){
+        super(stat);
+    }
+
+    @Override
+    public void run() {
+        try{
+            ResultSet res = stat.executeQuery();
+
+            while (res.next()) {
+                for (int i = 1; i < (res.getMetaData().getColumnCount()); i++)
+                    L.info(res.getString(i));
+                L.info("\n");
+            }
+
+        } catch (SQLException e) {
+            L.debug(e.getMessage() + " " + e.getErrorCode());
+            e.printStackTrace();
+        }
+    }
+}
