1ee8e34d1b73ca4f69c2cac118095e39f7ca3a97
Asius22 <ciro_2000@live.it>
2022-03-31 16:42:07 +0200

    configurazioni

--- src/main/java/jpa/Family.java
+++ src/main/java/jpa/Family.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 
-@Data
 @Entity
 public class Family {
     private String id, description;
--- src/main/java/jpa/Person.java
+++ src/main/java/jpa/Person.java
@@ -3,8 +3,6 @@ package jpa;
 import lombok.Data;
 
 import javax.persistence.Entity;
-
-@Data
 @Entity
 public class Person {
     private String id, name, lastName;
