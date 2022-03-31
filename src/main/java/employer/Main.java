593ef5e3a9e0198fc3975f1ef22df96fe195cb0f
Asius22 <ciro_2000@live.it>
2022-03-31 12:33:21 +0200

    aggiunta exception

--- src/main/java/employer/Main.java
+++ src/main/java/employer/Main.java
@@ -1,14 +1,18 @@
 package employer;
 
+import utils.Log;
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        Log l = Log.getInstance();
         try {
             CommandLine cmd = new CommandLine();
             cmd.start();
         } catch (IOException e){
-
+            l.error(e.getMessage());
+            e.printStackTrace();
         }
     }
 }
