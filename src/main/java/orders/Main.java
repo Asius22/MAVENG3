e66ebc291e0f15e8139333ddd3e9f6f615221dcd
Asius22 <ciro_2000@live.it>
2022-04-01 17:43:52 +0200

    provato persist

---
+++ src/main/java/orders/Main.java
@@ -0,0 +1,14 @@
+package orders;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona p = new Persona();
+        p.setLastName("Malafronte");
+        p.setName("Ciro");
+        p.setOrdini(new ArrayList<>());
+        PersonaUtils pu = new PersonaUtils();
+        pu.persist(p);
+    }
+}
