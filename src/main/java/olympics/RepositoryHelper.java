012995aaf28d0531d782965e05518de8117d9373
Asius22 <ciro_2000@live.it>
2022-04-01 09:46:59 +0200

    creato isnert

--- src/main/java/olympics/RepositoryHelper.java
+++ src/main/java/olympics/RepositoryHelper.java
@@ -20,7 +20,7 @@ public class RepositoryHelper implements BaseRepository<Athlete> {
                 "name varchar(20) not null, " +
                 "birthdate date not null, " +
                 "nation varchar(40) not null, " +
-                "height double not null, " +
+                "height float not null, " +
                 "primary key (code));";
 
         try {
@@ -40,7 +40,35 @@ public class RepositoryHelper implements BaseRepository<Athlete> {
 
     @Override
     public void persist(Athlete obj) {
+        String insert = "insert into athlete (name, nation, birthdate, height) " +
+                "values (?,?,?,?)";
 
+        try {
+            stat = conn.prepareStatement(insert);
+
+            stat.setString(1, obj.getName());
+            stat.setString(2, obj.getNation());
+            stat.setDate(3, obj.getBirthdate());
+            stat.setFloat(4, obj.getHeight());
+
+            print(stat.executeUpdate(), "insert");
+
+            commit();
+        } catch (SQLException e) {
+            l.error(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void commit() throws SQLException {
+        conn.commit();
+        stat.close();
+    }
+
+    private void print(int res, String method){
+        l.info(method.toUpperCase() + " : " +
+                        ((res > 0) ? "Riuscito" : "Riprova")
+                );
     }
 
     @Override
